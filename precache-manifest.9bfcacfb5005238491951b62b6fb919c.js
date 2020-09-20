4323872bce68c1e9e0644425cbcb2863e235f1a0
irfan738 <skismail23@yahoo.com>
2020-09-20 11:17:16 +0530

    Updates

---
+++ precache-manifest.9bfcacfb5005238491951b62b6fb919c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db9fbe4f2fb7be09a68c86823ca5e6b",
+    "url": "/product-detail-page/index.html"
+  },
+  {
+    "revision": "73e80dfce3e2839c3da8",
+    "url": "/product-detail-page/static/css/main.a3c6a0de.chunk.css"
+  },
+  {
+    "revision": "9509846dccbe07b0ee61",
+    "url": "/product-detail-page/static/js/2.5d84025f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/product-detail-page/static/js/2.5d84025f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e80dfce3e2839c3da8",
+    "url": "/product-detail-page/static/js/main.96a9c6e9.chunk.js"
+  },
+  {
+    "revision": "574f5c303ec0f729f65a",
+    "url": "/product-detail-page/static/js/runtime-main.8eaa80f2.js"
+  }
+]);
